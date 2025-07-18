c3cf936580063accc8605e3c31f6d0915fd84d12
Gedion Fetene <gedionfetene77@gmail.com>
2025-07-18 22:27:42 +0300

    Initial setup with components and interfaces folders

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,11 @@
-import Head from "next/head";
-
 const Home: React.FC = () => {
   return (
-    <>
-      <Head>
-        <title>Home | ALX Project 0x00</title>
-        <meta name="description" content="Welcome to your Next.js project" />
-      </Head>
-      <main className="flex items-center justify-center min-h-screen">
-        <h1 className="text-4xl font-bold text-blue-600">
-          🚀 Hello, ALX Project 0x00!
-        </h1>
-      </main>
-    </>
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Airbnb Application Clone system</h1>
+      <button className=" border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full">
+        Get Started
+      </button>
+    </main>
   );
 };
 
